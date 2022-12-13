bf620654831c41d15107dd7ac5056ea47226cae1
Miki Corral <miki@shopworld.es>
2022-12-13 11:22:03 +0100

    Add project files.

---
+++ MVVM/ViewModels/BMIViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace BMI.MVVM.ViewModels
+{
+    public class BMIViewModel
+    {
+        public Models.BMI BMI { get; set; }
+
+        public BMIViewModel()
+        {
+            BMI = new()
+            {
+                Weight = 80,
+                Height = 174
+            };
+        }
+    }
+}
